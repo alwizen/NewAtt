729bb4edada10e41143e7eb861112dfa86ab722c
alwizen <solu8i.project@gmail.com>
2026-02-21 21:41:44 +0700

    feat: add getRedirectUrl method to CreatePayrollDetail and EditPayrollDetail classes for consistent redirection

--- app/Filament/Resources/PayrollDetails/Pages/CreatePayrollDetail.php
+++ app/Filament/Resources/PayrollDetails/Pages/CreatePayrollDetail.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreatePayrollDetail extends CreateRecord
 {
     protected static string $resource = PayrollDetailResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
